8193e8114d6df563449883b0870c4ace7f851da1
jaylineweb <darkjay118@gmail.com>
2024-07-04 22:40:41 +0900

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -1,11 +1,3 @@
-// 유저가 값을 입력한다.
-// + 버튼을 클릭하면, 할일이 추가된다.
-// delete 버튼을 누르면 할 일이 삭제된다.
-// check 버튼을 누르면 할일이 끝나면서 밑줄이 간다.
-// 진행중 끝남 탭을 누르면, 언더바가 이동한다.
-// 끝남탭은, 끝난 아이템만, 진행중 탭은 진행중인 아이템만 나오게 된다.
-// 전체 탭을 누르면 다시 전체아이템으로 돌아옴
-
 let taskInput = document.getElementById('task-input'); // 유저가 값을 입력하는 input 창
 let addButton = document.getElementById('add-button'); // + 버튼 추출
 let taskBoard = document.getElementById('task-board');
@@ -108,5 +100,8 @@ function moveUnderLine(target) {
 }
 
 // 초기 렌더링
-renderTaskList();
-moveUnderLine(document.getElementById('all'));
+window.onload = function() {
+    renderTaskList();
+    let activeTab = document.querySelector('.tab.active');
+    moveUnderLine(activeTab);
+};
